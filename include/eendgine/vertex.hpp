7d1b40e8706b248069e5910d690849cf65abf7ef
david <david@karagory.com>
2023-10-18 16:55:58 -0500

    vertex in Eend namespace

--- include/eendgine/vertex.hpp
+++ include/eendgine/vertex.hpp
@@ -2,26 +2,27 @@
 
 #include<glm/glm.hpp>
 
-struct Vertex {
-    glm::vec3 position;
-    glm::vec4 color;
-    glm::vec2 uv;
-    glm::vec3 normal;
-    
-    void setPosition(float x, float y, float z) {
-        position = glm::vec3(x, y, z);
-    }
+namespace Eendgine {
+    struct Vertex {
+        glm::vec3 position;
+        glm::vec4 color;
+        glm::vec2 uv;
+        glm::vec3 normal;
+        
+        void setPosition(float x, float y, float z) {
+            position = glm::vec3(x, y, z);
+        }
 
-    void setColor(float r, float g, float b, float a) {
-        color = glm::vec4(r, g, b, a);
-    }
+        void setColor(float r, float g, float b, float a) {
+            color = glm::vec4(r, g, b, a);
+        }
 
-    void setUv(float u, float v) {
-        uv = glm::vec2(u, v);
-    }
+        void setUv(float u, float v) {
+            uv = glm::vec2(u, v);
+        }
 
-    void setNormal(float x, float y, float z) {
-        normal = glm::vec3(x, y, z); 
-    }
-};
-
+        void setNormal(float x, float y, float z) {
+            normal = glm::vec3(x, y, z); 
+        }
+    };
+}
