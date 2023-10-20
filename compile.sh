2fbbb4cc5a5d23fe022ab860f9e86c4b71d3d345
david <david@karagory.com>
2023-10-20 01:06:52 -0500

    forgot -Wall lol

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ -std=c++17 -o game.out -lSDL2 -lassimp -I include/ ./*.cpp ./glad/* ./eendgine/* ./stb/*
+g++ -Wall -std=c++17 -o game.out -lSDL2 -lassimp -I include/ ./*.cpp ./glad/* ./eendgine/* ./stb/*
