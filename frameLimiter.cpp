4f1eda7cfa250478bf76253158c1db51bb3d727d
David Karagory <david@karagory.com>
2025-02-16 23:51:22 -0500

    fix frame limiter and add min fps for logic

--- frameLimiter.cpp
+++ frameLimiter.cpp
@@ -6,21 +6,27 @@
 
 namespace Eendgine {
 
-void FrameLimiter::init(float fps) { _fps = fps; }
+void FrameLimiter::init(float maxFps, float minFps) {
+    _maxFps = maxFps;
+    _minFps = minFps;
+}
 
 void FrameLimiter::close() {}
 
-void FrameLimiter::startInterval() { _startTime = SDL_GetTicks(); }
+void FrameLimiter::startInterval() { _startTime = std::chrono::steady_clock::now(); }
 
 float FrameLimiter::stopInterval() {
-    const float intervalTime = SDL_GetTicks() - _startTime;
-    const float frameTime = 1.0f / _fps;
-    if (intervalTime < frameTime) {
-        std::this_thread::sleep_for(std::chrono::round<std::chrono::nanoseconds>(
-            std::chrono::duration<float>{frameTime - intervalTime}));
-        deltaTime = frameTime;
-    } else {
+    const auto intervalTime = std::chrono::duration_cast<std::chrono::microseconds>(
+        std::chrono::steady_clock::now() - _startTime)
+                                  .count();
+    const auto frameTime = std::chrono::microseconds((int)((1.0f / _maxFps) * 1000000.0f));
+    if (intervalTime < frameTime.count()) {
+        deltaTime =
+            std::chrono::duration_cast<std::chrono::microseconds>(frameTime).count() / 1000000.0f;
+    } else if (intervalTime < 1 / _minFps) {
         deltaTime = intervalTime;
+    } else {
+        deltaTime = 1 / _minFps;
     }
     return (float)intervalTime;
 }
--- frameLimiter.hpp
+++ frameLimiter.hpp
@@ -1,15 +1,19 @@
 #pragma once
 
+#include <chrono>
+
 namespace Eendgine {
 class FrameLimiter {
     public:
-        static void init(float fps);
+        static void init(float maxFps, float minFps);
         static void close();
         static void startInterval();
         static float stopInterval();
         inline static float deltaTime = 0; // sec
     private:
-        inline static float _startTime = 0;
-        inline static float _fps = 0;
+        inline static std::chrono::steady_clock::time_point _startTime =
+            std::chrono::steady_clock::now();
+        inline static float _maxFps = 0;
+        inline static float _minFps = 0;
 };
 } // namespace Eendgine
