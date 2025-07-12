72d80c1945a729452ae103e1269c622e4fe70dbc
David Karagory <david@karagory.com>
2025-07-13 00:44:00 +0200

    Entity batches some kinda singleton && tweak clang format

--- entityBatches.cpp
+++ entityBatches.cpp
@@ -1,16 +1,53 @@
 #include "entityBatches.hpp"
 
 namespace Eendgine {
+    
+void Entities::construct() {
+    assert(_statues == nullptr && _dolls == nullptr && _boards == nullptr && _panels == nullptr);
+    _statues = new EntityBatch<Statue>;
+    _dolls = new EntityBatch<Doll>;
+    _boards = new EntityBatch<Board>;
+    _panels = new EntityBatch<Panel>;
+}
+
+void Entities::destruct() {
+    assert(_statues != nullptr && _dolls != nullptr && _boards != nullptr && _panels != nullptr);
+    delete _statues;
+    delete _dolls;
+    delete _boards;
+    delete _panels;
+}
+
+EntityBatch<Statue>& Entities::getStatues() {
+    assert(_statues != nullptr);
+    return *_statues;
+}
+
+EntityBatch<Doll>& Entities::getDolls() {
+    assert(_dolls != nullptr);
+    return *_dolls;
+}
+
+EntityBatch<Board>& Entities::getBoards() {
+    assert(_boards != nullptr);
+    return *_boards;
+}
+
+EntityBatch<Panel>& Entities::getPanels() {
+    assert(_panels != nullptr);
+    return *_panels;
+}
+
 void Entities::draw(Shaders& shaders, Camera2D& hudCamera, Camera3D& sceneCamera) {
     glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    _statues.draw(shaders.getShader(Shader::STATUE), sceneCamera);
-    _dolls.draw(shaders.getShader(Shader::DOLL), sceneCamera);
-    _boards.draw(shaders.getShader(Shader::BOARD), sceneCamera);
+    _statues->draw(shaders.getShader(Shader::STATUE), sceneCamera);
+    _dolls->draw(shaders.getShader(Shader::DOLL), sceneCamera);
+    _boards->draw(shaders.getShader(Shader::BOARD), sceneCamera);
 
     glClear(GL_DEPTH_BUFFER_BIT);
 
-    _panels.draw(shaders.getShader(Shader::PANEL), hudCamera);
+    _panels->draw(shaders.getShader(Shader::PANEL), hudCamera);
 }
 } // namespace Eendgine
--- entityBatches.hpp
+++ entityBatches.hpp
@@ -8,8 +8,6 @@
 #include "shaders.hpp"
 #include "statue.hpp"
 
-#include <filesystem>
-
 namespace Eendgine {
 using StatueId = uint64_t;
 using DollId = uint64_t;
@@ -18,44 +16,24 @@ using PanelId = uint64_t;
 
 class Entities {
     public:
-        class StatueBatch {
-            public:
-                static StatueId insert(const std::filesystem::path statuePath) {
-                    return _statues.insert(statuePath);
-                };
-                static void erase(const StatueId id) { _statues.erase(id); };
-                static Statue* getRef(const StatueId id) { return _statues.getRef(id); }
-        };
-        class DollBatch {
-            public:
-                static DollId insert(const std::filesystem::path dollPath) {
-                    return _dolls.insert(dollPath);
-                };
-                static void erase(const DollId id) { _dolls.erase(id); };
-                static Doll* getRef(const DollId id) { return _dolls.getRef(id); }
-        };
-        class BoardBatch {
-            public:
-                static BoardId insert(const std::filesystem::path boardPath) {
-                    return _boards.insert(boardPath);
-                };
-                static void erase(const BoardId id) { _boards.erase(id); };
-                static Board* getRef(const BoardId id) { return _boards.getRef(id); }
-        };
-        class PanelBatch {
-            public:
-                static PanelId insert(const std::filesystem::path panelPath) {
-                    return _panels.insert(panelPath);
-                };
-                static void erase(const PanelId id) { _panels.erase(id); };
-                static Panel* getRef(const PanelId id) { return _panels.getRef(id); }
-        };
+        static void construct();
+        static void destruct();
+
+        static EntityBatch<Statue>& getStatues();
+        static EntityBatch<Doll>& getDolls();
+        static EntityBatch<Board>& getBoards();
+        static EntityBatch<Panel>& getPanels();
+
         static void draw(Shaders& shaders, Camera2D& hudCamera, Camera3D& sceneCamera);
 
     private:
-        inline static EntityBatch<Statue> _statues;
-        inline static EntityBatch<Doll> _dolls;
-        inline static EntityBatch<Board> _boards;
-        inline static EntityBatch<Panel> _panels;
+        Entities() = default;
+        ~Entities() = default;
+        
+        inline static EntityBatch<Statue>* _statues = nullptr;
+        inline static EntityBatch<Doll>* _dolls = nullptr;
+        inline static EntityBatch<Board>* _boards = nullptr;
+        inline static EntityBatch<Panel>* _panels = nullptr;
 };
+
 } // namespace Eendgine
