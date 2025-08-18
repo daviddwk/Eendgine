2207f51cc2ee3e1310710af73206f662ee9e4bb1
David Karagory <david@karagory.com>
2025-08-18 22:50:25 +0200

    random specific bit width

--- random.cpp
+++ random.cpp
@@ -1,5 +1,6 @@
 #include "random.hpp"
 #include <random>
+#include <cstdint>
 
 namespace Eendgine {
 
@@ -8,13 +9,13 @@ float randomRange(float min, float max) {
     return distribution(gen);
 }
 
-int randomRange(int min, int max) {
+int32_t randomRange(int32_t min, int32_t max) {
     std::uniform_int_distribution<int> distribution(min, max);
     return distribution(gen);
 }
 
-int randomIntLimit() {
-    return randomRange(std::numeric_limits<int>::min(), std::numeric_limits<int>::max());
+int32_t randomIntLimit() {
+    return randomRange(std::numeric_limits<int32_t>::min(), std::numeric_limits<int32_t>::max());
 }
 
 } // namespace Eendgine
