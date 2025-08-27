3aec698fdee277794d2a610094b89747f5c0ed12
David Karagory <david@karagory.com>
2025-08-27 19:14:52 +0200

    trim collision

--- collisionGeometry.cpp
+++ collisionGeometry.cpp
@@ -1,82 +1,15 @@
 #include "collisionGeometry.hpp"
-#include "fatalError.hpp"
-#include "loadModel.hpp"
 #include "types.hpp"
-#include "vertex.hpp"
 
 #include <cmath>
-#include <filesystem>
-#include <limits>
 #include <omp.h>
 
 namespace Eendgine {
 
-float sign(Point p1, Point p2, Point p3);
-Point triNormal(Point p1, Point p2, Point p3);
-bool vertOnTri(Point vert, std::array<Point, 3> tri);
-float pointHeightOnTri(const Point& p1, const Point& p2, const Point& p3, float x, float z);
-
 CollisionSphere::CollisionSphere(Point position, float radius)
     : position(position), radius(std::abs(radius)) {}
-CollisionPlane::CollisionPlane(Point position, Point normal)
-    : m_position(position), m_normal(normal) {}
-CollisionTriangle::CollisionTriangle(
-    std::array<Point, 3> vertPositions, std::array<Point, 3> vertNormals)
-    : m_verts(vertPositions) {
-    // getting face normal from vertex normals by getting average (I think the math is fine)
-    // https://math.stackexchange.com/questions/250165/converting-vertex-normals-to-face-normals
-    m_normal = glm::normalize(vertNormals[0] + vertNormals[1] + vertNormals[2]);
-    // for collision
-    if (m_normal.y > 0.1f) {
-        m_surface = surface::FLOOR;
-    } else if (m_normal.y < -0.1f) {
-        m_surface = surface::CEILING;
-    } else {
-        m_surface = surface::WALL;
-    }
-}
-CollisionCylinder::CollisionCylinder(Point position, float height, float radius)
-    : m_position(position), m_height(height), m_radius(radius) {}
-CollisionModel::CollisionModel(std::filesystem::path modelPath) {
-    std::vector<Vertex> vertices;
-    std::vector<unsigned int> indices;
-    loadModel(modelPath, vertices, indices);
-    if (indices.size() % 3 != 0) {
-        fatalError("collisionModel indices not divisible by 0");
-    }
-    for (unsigned int i = 0; i < indices.size(); i += 3) {
-        // TODO check for normals
-        m_collisionTris.emplace_back(
-            std::array<Point, 3>{vertices[indices[i]].position, vertices[indices[i + 1]].position,
-                vertices[indices[i + 2]].position},
-            std::array<Point, 3>{
-                vertices[indices[i]].normal,
-                vertices[indices[i + 1]].normal,
-                vertices[indices[i + 2]].normal,
-            });
-    }
-}
-/*
-bool colliding(CollisionSphere s1, CollisionSphere s2, Point* penetration) {
-    Point distance = s2.getPosition() - s1.getPosition();
-    float depth = (s1.getRadius() + s2.getRadius()) - glm::length(distance);
-    if (penetration != nullptr) {
-        *penetration = depth * glm::normalize(distance);
-    }
-    return depth > 0.0f;
-}
 
-bool colliding(CollisionSphere s, CollisionPlane p, Point* penetration) {
-    float distance = glm::dot(p.getNormal(), (s.getPosition() - p.getPosition()));
-    float depth = s.getRadius() - distance;
-    if (penetration != nullptr) {
-        Point pen = (-p.getNormal()) * depth;
-        *penetration = pen;
-    }
-    return depth > 0.0f;
-}
-*/
-bool colliding(Point2D point, CollisionRectangle rectangle) {
+bool pointOnRectangle(Point2D point, CollisionRectangle rectangle) {
     float behindLeft = point.x - rectangle.upperLeft.x; // negative means in front
     float behindTop = point.y - rectangle.upperLeft.y;
     float behindRight = rectangle.lowerRight.x - point.x;
@@ -87,7 +20,7 @@ bool colliding(Point2D point, CollisionRectangle rectangle) {
     return false;
 }
 
-std::optional<Point> colliding(Point point, CollisionSphere sphere) {
+std::optional<Point> pointToSphereEdgeRelative(Point point, CollisionSphere sphere) {
     Point difference = point - sphere.position;
     float distance = glm::length(difference);
     if (distance < sphere.radius) {
@@ -97,231 +30,18 @@ std::optional<Point> colliding(Point point, CollisionSphere sphere) {
     return std::nullopt;    
 }
 
-float snapCylinderToFloor(CollisionCylinder& c, CollisionTriangle& t) {
-    Point cylinderPos = c.getPosition();
-    std::array<Point, 3> triVerts = t.getVerts();
-    // TODO make not hardcoded like this
-    float snapDistance = 1.0f;
-    if (vertOnTri(cylinderPos, triVerts)) {
-        float triHeight =
-            pointHeightOnTri(triVerts[0], triVerts[1], triVerts[2], cylinderPos.x, cylinderPos.z);
-        // if slightly above floor OR clipping into floor
-        if (cylinderPos.y - triHeight <= snapDistance) {
-            return (triHeight + snapDistance) - cylinderPos.y;
-        }
-    }
-    return -1000000.0f;
-}
+float pointHeightOnTri(const Triangle& tri, const Point2D& point) {
+    // could use a tri construct here instead of 3 points
 
-Point pushCylinderFromWall(CollisionCylinder& c, CollisionTriangle& t) {
-    Point cylinderPos = c.getPosition();
-    Point triNormal = t.getNormal();
-    std::array<Point, 3> triVerts = t.getVerts();
-    // vector from cylinder to any point on triangle
-    Point toTri = triVerts[0] - cylinderPos;
-    // project vector onto plane of triangle with the tri's normal
-    // https://math.stackexchange.com/questions/3481232/projection-of-vector-v-onto-a-plane-with-normal-vector-n
-    Point projection = toTri * triNormal;
-    // if in triangular prisim (triangle with depth of radius)
-    // move to the face which the normal vector points toward
-    if (vertOnTri(projection + cylinderPos, t.getVerts()) &&
-        glm::length(projection) < c.getRadius()) {
-        float pushLength = c.getRadius() - glm::length(projection);
-        return pushLength * triNormal;
-    }
-    return Point(0.0f);
-}
-
-std::optional<float> pushCylinderFromCeiling(CollisionCylinder& c, CollisionTriangle& t) {
-    Point cylinderPos = c.getPosition() + c.getHeight();
-    std::array<Point, 3> triVerts = t.getVerts();
-    // TODO make not hardcoded like this
-    float snapDistance = 1.0f;
-    if (vertOnTri(cylinderPos, triVerts)) {
-        float triHeight =
-            pointHeightOnTri(triVerts[0], triVerts[1], triVerts[2], cylinderPos.x, cylinderPos.z);
-        // if slightly above floor OR clipping into floor
-        if (fabs(cylinderPos.y - triHeight) <= snapDistance) {
-            return {triHeight - (snapDistance + c.getHeight())};
-        }
-    }
-    return {};
-}
-
-CollisionResults adjustToCollision(CollisionCylinder& c, std::vector<CollisionModel*>& models) {
-    float hitFloor = -1 * std::numeric_limits<float>::infinity();
-    float hitWallX = 0.0f;
-    float hitWallZ = 0.0f;
-    std::optional<float> hitCeiling = {};
-
-    int numWalls = 0;
-
-    float cylinderHeight = c.getPosition().y;
-    float ceilingHeight = cylinderHeight;
-    float floorHeight = cylinderHeight;
-
-    float tmpFloorHeight = 0.0f;
-    Point tmpWallOffset(0.0f);
-
-    for (auto m : models) {
-        /*
-        #pragma omp parallel for private(tmpFloorHeight, tmpWallOffset)
-        reduction(+:numWalls, hitWallX, hitWallZ) reduction(max: hitFloor)
-        */
-        for (auto& t : m->getTris()) {
-            switch (t.getSurface()) {
-            case CollisionTriangle::surface::FLOOR:
-                tmpFloorHeight = snapCylinderToFloor(c, t);
-                if (tmpFloorHeight > floorHeight) {
-                    hitFloor = tmpFloorHeight;
-                }
-                break;
-            case CollisionTriangle::surface::WALL:
-                tmpWallOffset = pushCylinderFromWall(c, t);
-                if (tmpWallOffset != Point(0.0f)) {
-                    numWalls++;
-                } else {
-                    numWalls += 0;
-                }
-                hitWallX += tmpWallOffset.x;
-                hitWallZ += tmpWallOffset.z;
-                break;
-            case CollisionTriangle::surface::CEILING:
-                if (auto tmpCeilingHeight = pushCylinderFromCeiling(c, t)) {
-                    // #pragma omp critical
-                    if (*tmpCeilingHeight < ceilingHeight) {
-                        hitCeiling = *tmpCeilingHeight;
-                    }
-                }
-                break;
-            }
-        }
-    }
-    if (numWalls) {
-        hitWallX /= (float)numWalls;
-        hitWallZ /= (float)numWalls;
-    }
-    return CollisionResults{hitFloor == (-1 * std::numeric_limits<float>::infinity())
-                                ? std::nullopt
-                                : std::optional(hitFloor),
-        std::optional(Point(hitWallX, 0.0f, hitWallZ)), hitCeiling};
-}
-
-/*
-bool colliding(CollisionSphere s, CollisionTriangle &t, Point *penetration, Point position,
-Point scale) { Point closestPoint = closestTriPoint(s.getPosition(), t, position, scale);
-    Point distance = closestPoint - s.getPosition();
-    float depth = s.getRadius() - glm::length(distance);
-    if (penetration != nullptr) {
-        *penetration = depth * glm::normalize(distance);
-    }
-    return depth > 0.0f;
-}
-*/
-
-/*
-bool colliding(CollisionSphere s, CollisionModel &m, std::vector<Point> *penetration) {
-    bool collision = false;
-    auto triangles = m.getTris();
-    Point modelPos = m.getPosition();
-    Point modelScale = m.getScale();
-    Point tmpPen = Point(0.0f);
-    for ( int i = 0; i < triangles.size(); i++ ) {
-        if (colliding(s, triangles[i], &tmpPen, modelScale, modelPos)) {
-            penetration->push_back(tmpPen);
-            collision = true;
-        }
-    }
-    return collision;
-}
-*/
-/*
-Point closestTriPoint(Point p, CollisionTriangle t, Point position, Point scale) {
-    // thanks to Real-Time Collision Detection by Christer Ericson
-    auto verts = t.getVerts();
-    glm::vec3 a = (verts[0] * scale) + position;
-    glm::vec3 b = (verts[1] * scale) + position;
-    glm::vec3 c = (verts[2] * scale) + position;
-    glm::vec3 ab = verts[1] - verts[0];
-    glm::vec3 ac = verts[2] - verts[0];
-    glm::vec3 bc = verts[2] - verts[1];
-    // Compute parametric position s for projection P’ of P on AB,
-    // P’ = A + s*AB, s = snom/(snom+sdenom)
-    float snom = glm::dot(p - a, ab), sdenom = glm::dot(p - b, a - b);
-    // Compute parametric position t for projection P’ of P on AC,
-    // P’ = A + t*AC, s = tnom/(tnom+tdenom)
-    float tnom = glm::dot(p - a, ac), tdenom = glm::dot(p - c, a - c);
-    if (snom <= 0.0f && tnom <= 0.0f) return a; // Vertex region early out
-    // Compute parametric position u for projection P’ of P on BC,
-    // P’ = B + u*BC, u = unom/(unom+udenom)
-    float unom = glm::dot(p - b, bc), udenom = glm::dot(p - c, b - c);
-    if (sdenom <= 0.0f && unom <= 0.0f) return b; // Vertex region early out
-    if (tdenom <= 0.0f && udenom <= 0.0f) return c; // Vertex region early out
-    // P is outside (or on) AB if the triple scalar product [N PA PB] <= 0
-    glm::vec3 n = glm::cross(b - a, c - a);
-    float vc = glm::dot(n, glm::cross(a - p, b - p));
-    // If P outside AB and within feature region of AB,
-    // return projection of P onto AB
-    if (vc <= 0.0f && snom >= 0.0f && sdenom >= 0.0f) {
-        return a + snom / (snom + sdenom) * ab;
-    }
-    // P is outside (or on) BC if the triple scalar product [N PB PC] <= 0
-    float va = glm::dot(n, glm::cross(b - p, c - p));
-    // If P outside BC and within feature region of BC,
-    // return projection of P onto BC
-    if (va <= 0.0f && unom >= 0.0f && udenom >= 0.0f) {
-        return b + unom / (unom + udenom) * bc;
-    }
-    // P is outside (or on) CA if the triple scalar product [N PC PA] <= 0
-    float vb = glm::dot(n, glm::cross(c - p, a - p));
-    // If P outside CA and within feature region of CA,
-    // return projection of P onto CA
-    if (vb <= 0.0f && tnom >= 0.0f && tdenom >= 0.0f) {
-        return a + tnom / (tnom + tdenom) * ac;
-    }
-    // P must project inside face region. Compute Q using barycentric coordinates
-    float u = va / (va + vb + vc);
-    float v = vb / (va + vb + vc);
-    float w = 1.0f - u - v; // = vc / (va + vb + vc)
-    return u * a + v * b + w * c;
-}
-*/
-
-float sign(Point p1, Point p2, Point p3) {
-    return ((p1.x - p3.x) * (p2.z - p3.z)) - ((p2.x - p3.x) * (p1.z - p3.z));
-}
-float pointHeightOnTri(const Point& p1, const Point& p2, const Point& p3, float x, float z) {
-    // undefined behavior if plane is parallel
-    // WHAT?
-    // https://math.stackexchange.com/questions/1154340/how-to-find-the-height-of-a-2d-coordinate-on-a-3d-triangle
-    float a = -(p3.z * p2.y - p1.z * p2.y - p3.z * p1.y + p1.y * p2.z + p3.y * p1.z - p2.z * p3.y);
-    float b = (p1.z * p3.x + p2.z * p1.x + p3.z * p2.x - p2.z * p3.x - p1.z * p2.x - p3.z * p1.x);
-    float c = (p2.y * p3.x + p1.y * p2.x + p3.y * p1.x - p1.y * p3.x - p2.y * p1.x - p2.x * p3.y);
-    float d = -a * p1.x - b * p1.y - c * p1.z;
-    return -(a * x + c * z + d) / b;
-}
-
-Point triNormal(Point p1, Point p2, Point p3) {
+    // calc tri normal
     // https://www.khronos.org/opengl/wiki/Calculating_a_Surface_Normal
-    Point a = p2 - p1;
-    Point b = p3 - p1;
-
-    return Point(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x);
+    const Point u = tri.p2 - tri.p1;
+    const Point v = tri.p3 - tri.p1;
+    const Point normal = glm::cross(u, v);
+    // calc d for point normal plane
+    const float d = -((normal.x * tri.p1.x) + (normal.y * tri.p1.y) + (normal.z * tri.p1.z));
+    // solve for point.z
+    return -((normal.x * point.x) + (normal.y * point.y) + d) / normal.z;
 }
-bool vertOnTri(Point vert, std::array<Point, 3> tri) {
-    Point u = tri[1] - tri[0];
-    Point v = tri[2] - tri[0];
-    Point w = vert - tri[0];
-    float uu = glm::dot(u, u);
-    float uv = glm::dot(u, v);
-    float vv = glm::dot(v, v);
-    float wu = glm::dot(w, u);
-    float wv = glm::dot(w, v);
-    float denominator = uv * uv - uu * vv;
 
-    // Calculate barycentric coordinates
-    float s = (uv * wv - vv * wu) / denominator;
-    float t = (uv * wu - uu * wv) / denominator;
-    return (s >= 0) && (t >= 0) && (s + t <= 1);
-}
 } // namespace Eendgine
--- collisionGeometry.hpp
+++ collisionGeometry.hpp
@@ -1,17 +1,9 @@
 #pragma once
-#include <array>
-#include <filesystem>
 #include <optional>
-#include <vector>
 
 #include "types.hpp"
 
 namespace Eendgine {
-struct CollisionResults {
-        std::optional<float> floor;
-        std::optional<Point> wall;
-        std::optional<float> ceiling;
-};
 
 struct CollisionRectangle {
         Point2D upperLeft;
@@ -27,87 +19,8 @@ class CollisionSphere {
         float radius = 0;
 };
 
-class CollisionPlane {
-    public:
-        CollisionPlane(Point position, Point normal);
+bool pointOnRectangle(Point2D point, CollisionRectangle rectangle);
+float pointHeightOnTri(const Triangle& tri, const Point2D& point);
+std::optional<Point> pointToSphereEdgeRelative(Point point, CollisionSphere sphere);
 
-        void setPosition(float x, float y, float z) { m_position = Point(x, y, z); };
-        void setNormal(float x, float y, float z) { m_normal = Point(x, y, z); };
-
-        Point getPosition() { return m_position; };
-        Point getNormal() { return m_normal; };
-
-    private:
-        Point m_position;
-        Point m_normal;
-};
-
-class CollisionTriangle {
-    public:
-        enum surface { FLOOR, CEILING, WALL };
-        CollisionTriangle(std::array<Point, 3> vertPositions, std::array<Point, 3> vertNormals);
-        void setVerts(Point v0, Point v1, Point v2) { m_verts = {v0, v1, v2}; };
-        std::array<Point, 3> getVerts() { return m_verts; };
-        Point getNormal() { return m_normal; };
-        surface getSurface() { return m_surface; }
-
-    private:
-        std::array<Point, 3> m_verts;
-        Point m_normal;
-        surface m_surface;
-};
-
-class CollisionCylinder {
-    public:
-        CollisionCylinder(Point position, float height, float radius);
-
-        void setPosition(Point position) { m_position = position; };
-        void setHeight(float height) { m_height = height; };
-        void setRaduis(float radius) { m_radius = radius; };
-
-        Point getPosition() { return m_position; };
-        float getHeight() { return m_height; };
-        float getRadius() { return m_radius; };
-
-    private:
-        Point m_position;
-        float m_height;
-        float m_radius;
-};
-
-class CollisionModel {
-    public:
-        CollisionModel(std::filesystem::path modelPath);
-        void setPosition(Point position) { m_position = position; };
-        void setScale(Point scale) { m_scale = scale; };
-        Point getPosition() { return m_position; };
-        Point getScale() { return m_scale; };
-        std::vector<CollisionTriangle> getTris() {
-            std::vector<CollisionTriangle> tris = m_collisionTris;
-            for (auto& t : tris) {
-                std::array<Point, 3> triVerts = t.getVerts();
-                for (auto& v : triVerts) {
-                    v *= m_scale;
-                    v += m_position;
-                }
-                t.setVerts(triVerts[0], triVerts[1], triVerts[2]);
-            }
-            return tris;
-        };
-
-    private:
-        std::vector<CollisionTriangle> m_collisionTris;
-        Point m_position = Point(0.0f);
-        Point m_scale = Point(1.0f);
-};
-
-bool colliding(CollisionSphere s1, CollisionSphere s2, Point* penetration);
-bool colliding(CollisionSphere s, CollisionPlane p, Point* penetration);
-bool colliding(Point2D point, CollisionRectangle rectangle);
-std::optional<Point> colliding(Point point, CollisionSphere sphere);
-
-float snapCylinderToFloor(CollisionCylinder& c, CollisionTriangle& t);
-Point pushCylinderFromWall(CollisionCylinder& c, CollisionTriangle& t);
-std::optional<float> pushCylinderFromCeiling(CollisionCylinder& c, CollisionTriangle& t);
-CollisionResults adjustToCollision(CollisionCylinder& c, std::vector<CollisionModel*>& models);
 } // namespace Eendgine
