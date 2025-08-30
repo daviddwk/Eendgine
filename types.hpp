308b20281991f65782c767b254e4840047545fc8
David Karagory <david@karagory.com>
2025-08-30 10:18:49 +0200

    move out collision Geometry

--- types.hpp
+++ types.hpp
@@ -8,7 +8,8 @@ using Scale = glm::vec3;
 using Scale2D = glm::vec2;
 using Point = glm::vec3;
 using Point2D = glm::vec2;
-// should this be a vec3 for 3D???
+using Vector = glm::vec3;
+using Vector2D = glm::vec2;
 using Rotation = glm::vec3;
 using TransformationMatrix = glm::mat4;
 using Color = glm::vec4;
