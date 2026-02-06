7313ac49ce672df63ec5f7f19b68216dafe5eb80
David Karagory <david@karagory.com>
2026-02-06 18:40:20 +0100

    types now tile coords

--- types.hpp
+++ types.hpp
@@ -1,6 +1,6 @@
 #pragma once
-#include <glm/glm.hpp>
 #include <cmath>
+#include <glm/glm.hpp>
 
 namespace Eendgine {
 
@@ -10,6 +10,7 @@ using Scale = glm::vec3;
 using Scale2D = glm::vec2;
 using Point = glm::vec3;
 using Point2D = glm::vec2;
+using Tile = glm::vec2;
 using Vector = glm::vec3;
 using Vector2D = glm::vec2;
 using Rotation = glm::vec3;
@@ -18,8 +19,7 @@ using Color = glm::vec4;
 
 class Triangle {
     public:
-        Triangle(Point p1, Point p2, Point p3)
-        : p1(p1), p2(p2), p3(p3) {};
+        Triangle(Point p1, Point p2, Point p3) : p1(p1), p2(p2), p3(p3) {};
 
         Point p1;
         Point p2;
@@ -29,7 +29,7 @@ class Triangle {
 class Rectangle {
     public:
         Rectangle(Point2D upperLeft, Point2D lowerRight)
-        : upperLeft(upperLeft), lowerRight(lowerRight) {};
+            : upperLeft(upperLeft), lowerRight(lowerRight) {};
 
         Point2D upperLeft;
         Point2D lowerRight;
@@ -37,8 +37,7 @@ class Rectangle {
 
 class Sphere {
     public:
-        Sphere(Point position, float radius)
-        : position(position), radius(std::abs(radius)) {};
+        Sphere(Point position, float radius) : position(position), radius(std::abs(radius)) {};
 
         Point position = Point(0.0f, 0.0f, 0.0f);
         float radius = 0;
@@ -46,9 +45,7 @@ class Sphere {
 
 class Angle {
     public:
-        Angle(float degrees) {
-            setDegrees(degrees);
-        };
+        Angle(float degrees) { setDegrees(degrees); };
 
         float getDegrees() { return m_degrees; };
         void setDegrees(float degrees) {
@@ -59,30 +56,14 @@ class Angle {
             m_degrees = tmpDegrees;
         };
 
-        Angle operator+(const Angle& other) {
-            return Angle(m_degrees + other.m_degrees);
-        }
-        Angle operator-(const Angle& other) {
-            return Angle(m_degrees - other.m_degrees);
-        }
-        Angle operator*(const float factor) {
-            return Angle(m_degrees * factor);
-        }
-        Angle operator/(const float divisor) {
-            return Angle(m_degrees / divisor);
-        }
-        bool operator>(const Angle& other) {
-            return m_degrees > other.m_degrees;
-        }
-        bool operator>=(const Angle& other) {
-            return m_degrees >= other.m_degrees;
-        }
-        bool operator<(const Angle& other) {
-            return m_degrees < other.m_degrees;
-        }
-        bool operator<=(const Angle& other) {
-            return m_degrees <= other.m_degrees;
-        }
+        Angle operator+(const Angle& other) { return Angle(m_degrees + other.m_degrees); }
+        Angle operator-(const Angle& other) { return Angle(m_degrees - other.m_degrees); }
+        Angle operator*(const float factor) { return Angle(m_degrees * factor); }
+        Angle operator/(const float divisor) { return Angle(m_degrees / divisor); }
+        bool operator>(const Angle& other) { return m_degrees > other.m_degrees; }
+        bool operator>=(const Angle& other) { return m_degrees >= other.m_degrees; }
+        bool operator<(const Angle& other) { return m_degrees < other.m_degrees; }
+        bool operator<=(const Angle& other) { return m_degrees <= other.m_degrees; }
 
     private:
         float m_degrees = 0;
