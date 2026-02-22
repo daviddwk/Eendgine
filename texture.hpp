7c23edc4c22422c091ba3200358dc141716dc7eb
David Karagory <david@karagory.com>
2026-02-22 13:43:04 +0100

    move texture stuff into texture cache

--- texture.hpp
+++ texture.hpp
@@ -1,17 +1,10 @@
 #pragma once
-#include <GLES3/gl3.h>
-#include <filesystem>
+#include "textureCache.hpp"
 
 namespace Eendgine {
-struct Texture {
-        GLuint id = 0;
-        int width = 0;
-        int height = 0;
-};
 struct Strip {
         Texture texture;
         size_t len;
 };
 
-Texture loadTexture(std::filesystem::path filePath);
 } // namespace Eendgine
--- textureCache.hpp
+++ textureCache.hpp
@@ -1,14 +1,26 @@
 #pragma once
-#include "texture.hpp"
+#include <GLES3/gl3.h>
 #include <filesystem>
 #include <map>
 
 namespace Eendgine {
+
+// TODO make a way to unload textures (at least in destructor)
+// and make a singleton like everything else
+
+struct Texture {
+        GLuint id = 0;
+        int width = 0;
+        int height = 0;
+};
+
 class TextureCache {
     public:
         static Texture getTexture(std::filesystem::path texturePath);
 
     private:
+        static Texture loadTexture(std::filesystem::path filePath);
         inline static std::map<std::filesystem::path, Texture> m_textureMap;
 };
+
 } // namespace Eendgine
