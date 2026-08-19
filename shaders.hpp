3bc81f0981df034df1fd4184592c05f557730ccb
David Karagory <david@karagory.com>
2026-08-19 22:14:01 +0200

    shader consts

--- shaders.hpp
+++ shaders.hpp
@@ -3,7 +3,22 @@
 #include "shader.hpp"
 
 namespace Eendgine {
+
+const std::string SHADER_PATH_PANEL_VERT = "shaders/panel.vert";
+const std::string SHADER_PATH_PANEL_FRAG = "shaders/panel.frag";
+const std::string SHADER_PATH_BOARD_VERT = "shaders/board.vert";
+const std::string SHADER_PATH_BOARD_FRAG = "shaders/board.frag";
+const std::string SHADER_PATH_STATUE_VERT = "shaders/statue.vert";
+const std::string SHADER_PATH_STATUE_FRAG = "shaders/statue.frag";
+const std::string SHADER_PATH_DOLL_VERT = "shaders/doll.vert";
+const std::string SHADER_PATH_DOLL_FRAG = "shaders/doll.frag";
+const std::string SHADER_PATH_SCREEN_VERT = "shaders/screen.vert";
+const std::string SHADER_PATH_SCREEN_FRAG = "shaders/screen.frag";
+
+const std::string SHADER_SCREEN_INT_PIXEL_SIZE = "pixelSize";
+
 enum class Shader { panel, board, statue, doll, screen };
+
 class Shaders {
     public:
         static void construct(
